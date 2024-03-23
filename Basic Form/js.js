8ef0d4507a739be199a27f812f0aca924107e541
adityarevanth <adityarevanth@lyrostech.com>
2024-03-23 11:27:22 +0530

    commit

--- Basic Form/js.js
+++ Basic Form/js.js
@@ -1,21 +1,21 @@
+let form = document.querySelector("form");
+let name_box = document.querySelector("#name");
+let email_box = document.querySelector("#email");
+let password_box = document.querySelector("#password");
+let confirm_password_box = document.querySelector("#confirm-password");
+let output = document.querySelector(".output");
 
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    ValidateForm(); 
+});
 
-let form=document.querySelector("form")
-let name_box=document.querySelector("#name");
-let email_box=document.querySelector("#email");
-let passwoed_box=document.querySelector("password");
-let confirm_password_box=document.querySelector("#confirm-password");
-let btn=document.querySelector("button");
-let output=document.querySelector(".output");
-
-btn.addEventListener("click", ValidateForm)
-
-function ValidateForm(){
-    let name=name_box.value;
-    let email=email_box.value;
-    let password=passwoed_box.value;
-    let confirm_password=passwoed_box.value;
-    console.log(name,email,password,confirm_password);
-
-    output.innerHTML=`name:${name} Email:${email} password:${password} confirm_password:${confirm_password}`
+function ValidateForm() {
+    
+    let name = name_box.value;
+    let email = email_box.value;
+    let password = password_box.value;
+    let confirm_password = confirm_password_box.value; 
+     console.log(name, email, password, confirm_password);
+    output.innerHTML = `Name: ${name} <br> Email: ${email} <br> Password: ${password} <br> Confirm Password: ${confirm_password}`;
 }
