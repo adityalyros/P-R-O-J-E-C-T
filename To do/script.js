85c03a8ccc5c4978661ca7acccd02acc85bf14c9
adityarevanth <adityarevanth@lyrostech.com>
2024-03-27 11:21:07 +0530

    todo1

--- To do/script.js
+++ To do/script.js
@@ -1,11 +1,14 @@
 let addCardBtn = document.querySelector("#addCard");
 let todoContainer = document.querySelector("#todo");
-addCardBtn.addEventListener("click",addTask);
+addCardBtn.addEventListener("click", addTask);
 
 function addTask(){
-    let card=document.createElement("div")
-    card.className="card"
-    card.innerText="Test Card"
-    card.setAttribute("contenteditable",true) //pointer will be editable zone automatically.
-    todoContainer.append()
+    let card = document.createElement("div");
+    card.className = "card";
+    card.innerText = "Test Card";
+    card.setAttribute("contenteditable", "true");
+    todoContainer.append(card);
+
+    //problem 1 empty card should automatically be removed
+    
 }
